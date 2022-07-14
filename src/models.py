324e42c5a217a0bcedd098fa665a78e1934dca44
Manuel Martret <manulm27@hotmail.com>
2022-07-14 21:29:46 +0000

    built data model starwars

--- src/models.py
+++ src/models.py
@@ -8,23 +8,60 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class Characters(Base):
+    __tablename__ = 'characters' 
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    gender = Column(String(250))
+    skin_color = Column(String(250))
+    birth_year = Column(String(250))
+    created = Column(String(250))
+    mass = Column(Integer)
+    height = Column(Integer)
+    homeworld = Column(String(250), ForeignKey('planets.id'))
+    specie = Column(String(250), ForeignKey("specie.id"))
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+
+class Planets(Base):
+    __tablename__ = 'planets'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250),  nullable=False)
+    diameter = Column(Integer)
+    rotation_period = Column(Integer, nullable=False)
+    orbital_period = Column(Integer, nullable=False)
+    terrain = Column(String(250))
+    climate = Column(String(250))
+    surface_water = Column(Integer)
+    created = Column(Integer)
+    population = Column(Integer)
+    aircraft_belonging = Column(String(250), ForeignKey('starships.id'))
+    vehicle_belonging = Column(String(250), ForeignKey('vehicle.id'))
+    characters = relationship(Characters)
+
+class Starships(Base):
+    __tablename__ = 'starships'
+    id = Column(Integer, primary_key=True)
+    velocity = Column(Integer)
+    pilot = Column(Integer, ForeignKey('characters.id')) 
+    created = Column(Integer)
+    model = Column(String(250), nullable=False)
+
+class Vehicle(Base):
+    __tablename__ = 'vehicle'
+    id = Column(Integer, primary_key=True)
+    velocity = Column(Integer)
+    pilot = Column(Integer, ForeignKey('characters.id')) 
+    created = Column(Integer)
+    model = Column(String(250), nullable=False)
+
+class Specie(Base):
+    __tablename__ = "specie"
+    id = Column(Integer, primary_key=True)
+    character_id = Column(Integer, ForeignKey('characters.id'))
+    classification = Column(String(250))
+    designation = Column(String(250))
+    language = Column(String(250))
+    characters = relationship(Characters)
 
     def to_dict(self):
         return {}
